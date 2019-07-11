cb8fe7ec0d9097f02335b48eebdbd48e3c3b2889
josias-r <josias.ribi@gmail.com>
2019-07-11 10:01:19 +0200

    added script and style enqueues

---
+++ admin/assets/editor.js
@@ -0,0 +1,7 @@
+/* global wp */
+
+wp.domReady(() => {
+  // wp.blocks.unregisterBlockStyle("core/button", "default");
+  // wp.blocks.unregisterBlockStyle("core/button", "outline");
+  // wp.blocks.unregisterBlockStyle("core/button", "squared");
+});
